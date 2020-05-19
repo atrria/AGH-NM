65d783ed09f7e1430351c1bb4af457d1bb3a4778
Adrianna <ada.lesna@gmail.com>
2020-05-19 10:41:31 +0200

    Lab11 updated

--- Lab11/chart_script.py
+++ Lab11/chart_script.py
@@ -3,14 +3,74 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 
-file = "results1.txt"
-data = np.loadtxt(file)
+file1 = "results1_10.txt"
+file2 = "results2_10.txt"
+file3 = "results3_10.txt"
+file4 = "results3_8.txt"
+file5 = "results3_6.txt"
 
-x=data[:,0]
-y=data
+data1 = np.loadtxt(file1)
+data2 = np.loadtxt(file2)
+data3 = np.loadtxt(file3)
+data4 = np.loadtxt(file4)
+data5 = np.loadtxt(file5)
 
-plt.plot(x,y[:,2], color="red")
-plt.plot(x,y[:,1], color="black")
+x=data1[:,0]
+y=data1[:,1]
 
-#plt.show()
-plt.savefig("chart1.png")
+plt.plot(x, y, color='#104176', linewidth=1)
+plt.title("k=10, sygnał zaszumiony")
+plt.xlabel("i")
+plt.savefig("chart1_10.png")
+
+plt.clf()
+
+x=data2[:,0]
+y=data2[:,2]
+
+plt.plot(x, y, color='#104176', linewidth=1)
+plt.title("k=10, transformata Fouriera")
+plt.xlabel("$\omega$")
+plt.savefig("chart2_10.png")
+
+plt.axis([0,100,-100,600])
+plt.savefig("chart2b_10.png")
+
+plt.clf()
+
+x=data3[:,0]
+y=data3
+
+plt.plot(x,y[:,1], color='#ff3333')
+plt.plot(x,y[:,2], color='#104176', linewidth=1)
+plt.title("k=10")
+plt.xlabel("i")
+plt.legend(["sygnał niezaszumiony","sygnał odszumiony"])
+plt.savefig("chart3_10.png")
+
+plt.clf()
+
+x=data4[:,0]
+y=data4
+
+plt.plot(x,y[:,2], color='#104176', linewidth=1)
+plt.plot(x,y[:,1], color='#ff3333')
+plt.title("k=8")
+plt.xlabel("i")
+plt.legend(["sygnał odszumiony","sygnał niezaszumiony"])
+plt.savefig("chart3_8.png")
+
+plt.clf()
+
+x=data5[:,0]
+y=data5
+
+plt.plot(x,y[:,2], color='#104176', linewidth=1)
+plt.plot(x,y[:,1], color='#ff3333')
+plt.title("k=6")
+plt.xlabel("i")
+plt.legend(["sygnał odszumiony","sygnał niezaszumiony"])
+plt.savefig("chart3_6.png")
+
+
+
--- Lab11/sinus_transformation.cpp
+++ Lab11/sinus_transformation.cpp
@@ -1,126 +1,143 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <gsl\gsl_linalg.h>
-#include <gsl\gsl_fft_real.h>
-#include <gsl\gsl_fft.h>
-#include <gsl\gsl_fft_halfcomplex.h>
 #include <fstream>
 #include <algorithm>
+#include <string>
 
-/*
-    Nie jestem przekonana czy u¿ywam dobrych funkcji FFT.
-    Jeœli u¿yjê gsl_fft_real_transform lub gsl_fft_real_radix2_transform to pierwszy wykres wydaje siê byæ dobry (odbity wzglêdem osi X)
-    jednak nie istniej¹ analogiczne funkcje obliczaj¹ce transformatê odwrotn¹.
-    Funkcja gsl_fft_halfcomplex wydaje siê byæ tym czego szukam, jednak wyniki z niej wygl¹daj¹ ju¿ niepoprawnie.
-*/
+#define _USE_MATH_DEFINES
+#include <math.h>
 
+#define NRANSI
+extern "C"
+{
+    #include "nrutil.h"
+}
+#undef NRANSI
+
+#include "lib/sinft.c"
+#include "lib/realft.c"
+#include "lib/four1.c"
+
+// signum, zwraca 1 lub -1
 int signum(double Y)
 {
-    if (Y > 0.5)
+    return (Y > 0.5f) ? 1 : -1;
+}
+
+// wartoœæ sygna³u niezaszumionego
+float y0_value(const int n, int i)
+{
+    float omega = 4.f * (float)(M_PI) / n;
+    return std::sin(omega * i) + std::sin(2.f * omega * i) + std::sin(3.f * omega * i);
+}
+
+// szum
+float a_rand()
+{
+    float X = rand() / (RAND_MAX + 1.f);
+    float Y = rand() / (RAND_MAX + 1.f);
+    return 2.f * signum(Y) * X;
+}
+
+// wartoœæ sygna³u zaszumionego, zapisywana do pliku
+void signal(const int n, float* y, std::ofstream& file)
+{
+    for (int i = 1; i <= n; i++)
     {
-        return 1;
+        y[i] = y0_value(n, i) + a_rand();
+        file << i << "\t" << y[i] << std::endl;
     }
-    return -1;
 }
 
-double a_rand()
+// najwiêksza wartoœæ w wektorze
+double max_in_y(const int n, float* y)
 {
-    double X = rand() / (RAND_MAX + 1.);
-    double Y = rand() / (RAND_MAX + 1.);
-
-    return 2. * signum(Y) * X;
-}
-
-void signal(const int n, gsl_vector* y, std::ofstream& file1)
-{
-    double omega = 4. * M_PI / n;
-    for (int i = 0; i < n; i++)
+    double max = y[1];
+    for (int i = 2; i <= n; i++)
     {
-        gsl_vector_set(y, i, std::sin(omega * i) + std::sin(2 * omega * i) + std::sin(3 * omega * i) + a_rand());
-        file1 << i << "\t" << std::sin(omega * i) + std::sin(2 * omega * i) + std::sin(3 * omega * i) << "\t" << gsl_vector_get(y, i) << std::endl;
-    }
-}
-
-double max_in_y(const int n, gsl_vector* y)
-{
-    double max = gsl_vector_get(y, 0);
-    for (int i = 1; i < n; i++)
-    {
-        if (gsl_vector_get(y, i) > max)
+        if (y[i] > max)
         {
-            max = gsl_vector_get(y, i);
+            max = y[i];
         }
     }
     return max;
 }
 
-int main()
+void discrimination_threshold(const int n, float* y)
 {
-    srand(time(NULL));
-
-    std::ofstream file1("results1.txt");
-    std::ofstream file2("results2.txt");
-    std::ofstream file3("results3.txt");
-
-    const int k = 10;
-    const int n = std::pow(2, k);
-
-    auto y = gsl_vector_calloc(n);
-
-    signal(n, y, file1);
-
-    gsl_fft_halfcomplex_wavetable* wavetable = gsl_fft_halfcomplex_wavetable_alloc(n);
-    gsl_fft_real_workspace* workspace = gsl_fft_real_workspace_alloc(n);
-
-    gsl_fft_halfcomplex_wavetable* wavetable2 = gsl_fft_halfcomplex_wavetable_alloc(n);
-    gsl_fft_real_workspace* workspace2 = gsl_fft_real_workspace_alloc(n);
-
-    //gsl_fft_real_transform(y->data, y->stride, n, wavetable, workspace);
-    //gsl_fft_real_radix2_transform(y->data, y->stride, n);
-    gsl_fft_halfcomplex_transform(y->data, y->stride, n, wavetable, workspace);
-
-    for (int i = 0; i < n; i++)
-    {
-        file2 << gsl_vector_get(y, i) << "\n";
-    }
-
     double max = max_in_y(n, y);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if (std::fabs(gsl_vector_get(y, i)) < (0.25 * max))
+        if (std::fabs(y[i]) <= (0.25f * max)) // niestety nie jestem pewna jaki znak powinien siê tu znajdowaæ - '<' czy '<='
         {
-            gsl_vector_set(y, i, 0.);
+            y[i] = 0.f;
         }
     }
+}
 
-    //gsl_fft_real_transform(y->data, y->stride, n, wavetable, workspace);
-    gsl_fft_halfcomplex_inverse(y->data, y->stride, n, wavetable2, workspace2);
-
-    for (int i = 0; i < n; i++)
+void multiply(const int n, float* y)
+{
+    for (int i = 1; i <= n; i++)
     {
-        gsl_vector_set(y, i, gsl_vector_get(y, i) * (2. / n));
+        y[i] *= (2.f / n);
     }
+}
 
-    for (int i = 0; i < n; i++)
+// zapis wartoœci zaszumionych/odszumionych do pliku
+void y_to_file(const int n, float* y, std::ofstream& file)
+{
+    for (int i = 1; i <= n; i++)
     {
-        file3 << gsl_vector_get(y, i) << "\n";
+        file << i << "\t" << y0_value(n, i) << "\t" << y[i] << "\n";
     }
+}
 
-    gsl_vector_free(y);
-    gsl_fft_halfcomplex_wavetable_free(wavetable);
-    gsl_fft_halfcomplex_wavetable_free(wavetable2);
-    gsl_fft_real_workspace_free(workspace);
-    gsl_fft_real_workspace_free(workspace2);
+void FFT(int k)
+{
+    std::ofstream file1("results1_" + std::to_string(k) + ".txt");
+    std::ofstream file2("results2_" + std::to_string(k) + ".txt");
+    std::ofstream file3("results3_" + std::to_string(k) + ".txt");
+
+    const int n = (const int)(std::pow(2, k));
+    float* y = new float[n];
+
+    // zapisanie zaszumionego sygna³u do wektora typu float i do pliku
+    signal(n, y, file1);
+
+    // transformata sinusowa sygna³u
+    sinft(y, n);
+
+    // zapisanie wektora do pliku
+    y_to_file(n, y, file2);
+
+    // dyskryminacja na poziomie 25% wartoœci maksymalnej
+    discrimination_threshold(n, y);
+
+    // odwrotna transformata sinusowa
+    sinft(y, n);
+
+    // przemno¿enie sygna³u
+    multiply(n, y);
+
+    // zapisanie wektora do pliku
+    y_to_file(n, y, file3);
 
     file1.close();
     file2.close();
     file3.close();
+}
+
+int main()
+{
+    srand((unsigned int)(time)(NULL));
+
+    FFT(6);
+    FFT(8);
+    FFT(10);
 
     system(".\\chart_script.py");
-    system(".\\chart_script2.py");
-    system(".\\chart_script3.py");
 
     return 0;
 }
